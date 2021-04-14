4e51ff83e5dd85c10cbbe7c33db381e6adc81d20
Niki <nikimortazavi21@gmail.com>
2021-04-14 11:24:00 +0200

    Fix spacing

--- src/components/LiveAnywhere.js
+++ src/components/LiveAnywhere.js
@@ -1,39 +1,53 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import {
+  Card,
   CardActionArea,
   CardContent,
   CardMedia,
   Typography,
 } from "@material-ui/core";
-import "../styles/Home.css";
-import cardData from '../data/liveAnywhere.json'
-import '../styles/LiveAnywhere.css'
+import cardData from "../data/liveAnywhere.json";
+import "../styles/LiveAnywhere.css";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    boxShadow: "none",
+    borderRadius: "7px",
+  },
+  media: {
+    height: "326px",
+    width: "326px",
+    borderRadius: "7px",
+  },
+  text: {
+    padding: 5,
+  },
+});
 
 const LiveAnywhere = () => {
-    const [data, setData] = useState(cardData)
+  const [data, setData] = useState(cardData);
+  const classes = useStyles();
+
   return (
     <div className="live-anywhere">
       <h1 className="title">Live anywhere</h1>
       <div className="card-section">
         {data.map((item) => (
-          <CardActionArea className="cards">
-            <CardMedia
-              className="card-imgs"
-              component="img"
-              image={item.img}
-              title={item.title}
-            />
-            <CardContent>
-              <Typography
-                className="card-text"
-                gutterBottom
-                variant="h5"
-                component="h2"
-              >
-                {item.title}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
+          <Card className={classes.root}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image={item.img}
+                title={item.title}
+              />
+              <CardContent className={classes.text}>
+                <Typography className="card-text" gutterBottom variant="h5">
+                  {item.title}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
         ))}
       </div>
     </div>
